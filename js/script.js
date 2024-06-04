c1a4b89b3724e4319cbc82fe5a013d967ec747a6
Julia <daedra2632@gmail.com>
2024-06-04 16:52:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+///alert('Hello');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = prompt("Вам есть уже 18?", "18")
+//console.log(typeof(answer));  //typeof - показывает какой тип данных лежит в переменной answer
+//Ответ: string   (все ответы от пользователей всегда только в строчном формате)
+
+//const answers = []; //переменная answers в которую поместили пустой масив
+
+//answers[0] = prompt('Сколько вам лет?', '');
+//answers[1] = prompt('Как ваше имя?', '');
+//answers[2] = prompt('Как ваша фамилия?', '');
+
+//document.write(answers);
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10;
+
+incr++;
+alert(incr);
+
